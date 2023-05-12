35c9bd5ef573caf9a0fbeed8891ec07bd6f0354e
Alphonce08 <alphoncerotich30@gmail.com>
2023-05-12 16:48:07 +0300

    navigation layout

--- app/src/main/java/com/example/simplenavigateapp/MainActivity.kt
+++ app/src/main/java/com/example/simplenavigateapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.simplenavigateapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -7,13 +8,19 @@ import android.widget.Button
 class MainActivity : AppCompatActivity() {
 
     lateinit var home:Button
+    lateinit var about:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        home = findViewById(R.id.btnhome)
+        about = findViewById(R.id.btnabout)
+        about.setOnClickListener {
+            var intent = Intent(this, About::class.java)
+            startActivity(intent)
+        }
 
+        home = findViewById(R.id.btnhome)
         home.setOnClickListener {
             var intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
